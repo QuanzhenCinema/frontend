89703f7ec30941110aeeb352249253d48fee9935
Winely <sherlockheroine@gmail.com>
2016-07-18 15:50:52 +0800

    更新readme

--- js/staff.js
+++ js/staff.js
@@ -19,7 +19,7 @@ var TableInit = function () {
     //初始化Table
     oTableInit.Init = function () {
         $('#tb_departments').bootstrapTable({
-            url: '/Home/GetDepartment',         //请求后台的URL（*）
+            url: '/Home/GetStaff',         //请求后台的URL（*）
             method: 'get',                      //请求方式（*）
             toolbar: '#toolbar',                //工具按钮用哪个容器
             striped: true,                      //是否显示行间隔色
